9937e606d3aeb9f1d800b1680e7adad8dc87da1d
felixagure50-cs <felixagure50@gmail.com>
2026-03-18 08:16:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python
+import subprocess
+import sys
 
+def setup():
+    print("Installing setuptools...")
+    subprocess.run([sys.executable, "-m", "pip", "install", "--upgrade", "setuptools==65.5.0"])
+    print("Setup complete!")
+
+if __name__ == "__main__":
+    setup()
